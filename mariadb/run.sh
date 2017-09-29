0a46ada7705336bbac650be95f1db777d04b337d
mardinw <mardinwijaya46@gmail.com>
2017-09-29 16:26:14 +0700

    change konfig

--- mariadb/run.sh
+++ mariadb/run.sh
@@ -5,8 +5,11 @@ VOLUME_HOME="/var/lib/mysql"
 if [[ ! -d $VOLUME_HOME/mysql ]]; then
     echo "=> An empty or uninitialized MariaDB volume is detected in $VOLUME_HOME"
     echo "=> Installing MariaDB ..."
-    mysql_install_db > /dev/null 2>&1
-    echo "=> Done!"  
+
+    chown -R mysql:mysql /var/lib/mysql
+
+    mysql_install_db --user=mysql > /dev/null 2>&1
+    echo "=> Done!"
     sh mariadb_init.sh
 else
     echo "=> Using an existing volume of MariaDB"
