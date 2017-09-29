64905fbade5751838567a8da0e62f847eb862677
mardinw <mardinwijaya46@gmail.com>
2017-09-29 16:31:22 +0700

    back to ori

--- mariadb/mariadb_init.sh
+++ mariadb/mariadb_init.sh
@@ -4,50 +4,19 @@
 
 mysqladmin --silent --wait=30 ping
 
-if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
-    MYSQL_ROOT_PASSWORD=`pwgen 16 1`
-    echo "[i] MySQL root Password: $MYSQL_ROOT_PASSWORD"
-fi
-
-MYSQL_DATABASE=${MYSQL_DATABASE:-""}
-MYSQL_USER=${MYSQL_USER:-""}
-MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
-
-tfile=`mktemp`
-if [ ! -f "$tfile" ]; then
-    return 1
-fi
-
-cat <<EOF > $tfile
-USE mysql;
-FLUSH PRIVILEGES;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
-UPDATE user SET password=PASSWORD("$MYSQL_ROOT_PASSWORD") WHERE user='root';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-UPDATE user SET password=PASSWORD("") WHERE user='root' AND host='localhost';
-EOF
-
-if [ "$MYSQL_DATABASE" != "" ]; then
-    echo "[i] Creating database: $MYSQL_DATABASE"
-    echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
-
-    if [ "$MYSQL_USER" != "" ]; then
-        echo "[i] Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
-        echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
-    fi
-fi
-
-/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 < $tfile
-rm -f $tfile
-
-#mysql -uroot -e "CREATE USER 'din'@'%' IDENTIFIED BY 'rahasia'"
-#mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'din'@'%' WITH GRANT OPTION"
-#mysql -uroot -e "FLUSH PRIVILEGES"
-#mysql -uroot -e "CREATE SCHEMA din"
+mysql -uroot -e "CREATE USER 'din'@'%' IDENTIFIED BY 'rahasia'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'din'@'%' WITH GRANT OPTION"
+mysql -uroot -e "FLUSH PRIVILEGES"
+mysql -uroot -e "CREATE SCHEMA din"
 
 echo "=> Done!"
 
 echo "========================================================================"
+echo "You can now connect to this MariaDB Server using:"
+echo ""
+echo "    mysql -udin -prahasia -h<host> -P<port>"
+echo ""
+echo "Please remember to change the above password as soon as possible!"
 echo "MariaDB user 'root' has no password but only allows local connections"
 echo "========================================================================"
 
