7606b5eb57354cf1e77652dae74a5f904fa88306
mardinw <mardinwijaya46@gmail.com>
2017-09-30 17:30:01 +0700

    initial commit

--- alpine-nginx-phpfpm/etc/mariadb_init.sh
+++ alpine-nginx-phpfpm/etc/mariadb_init.sh
@@ -4,18 +4,15 @@
 
 mysqladmin --silent --wait=30 ping
 
-mysql -uroot -e "CREATE USER 'din'@'%' IDENTIFIED BY 'rahasia'"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'din'@'%' WITH GRANT OPTION"
+mysql -uroot -e "CREATE USER 'mardin'@'%' IDENTIFIED BY 'udin4j4'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'mardin'@'%' WITH GRANT OPTION"
 mysql -uroot -e "FLUSH PRIVILEGES"
-mysql -uroot -e "CREATE SCHEMA homesch"
+mysql -uroot -e "CREATE SCHEMA homestead"
 
 echo "=> Done!"
 
 echo "========================================================================"
-echo "You can now connect to this MariaDB Server using:"
-echo ""
-echo "    mysql -udin -prahasia -h<host> -P<port>"
-echo ""
+echo "You can now connect to this MariaDB Server"
 echo "Please remember to change the above password as soon as possible!"
 echo "MariaDB user 'root' has no password but only allows local connections"
 echo "========================================================================"
